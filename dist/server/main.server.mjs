6a2a66accba5da83f89f693355fc471db7c28ecd
Akash Suryawanshi <s.akashb@gmail.com>
2025-05-04 23:58:16 +0530

    kept only dist folder content

--- dist/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-2Y7RP4C6.mjs";import"./chunk-UTEOTZSS.mjs";import"./chunk-RGNWAOKM.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { InvoiceGeneratorComponent } from './invoice-generator/components/invoice-generator/invoice-generator.component';
-import { GenieInvoiceComponent } from './invoice-generator/components/genie-invoice/genie-invoice.component';
-
-export const routes: Routes = [
-  { path: '', component: InvoiceGeneratorComponent },
-  { path: 'genie-invoice', component: GenieInvoiceComponent },
-  { path: '**', redirectTo: '' }
-];
